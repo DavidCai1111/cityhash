aa6a697b8929a9134aa063158d48786db5c63d58
David Cai <davidcai1993@yahoo.com>
2018-01-05 16:48:48 +0800

    feat: add stringifyObject()

--- index.js
+++ index.js
@@ -2,15 +2,34 @@
 const cityhash = require('bindings')('cityhash')
 
 function hash32 (data) {
-  if (typeof data !== 'string') throw new Error(`${data} should be string`)
-
-  return cityhash.hash32(data)
+  return cityhash.hash32(stringifyObject(data))
 }
 
 function hash64 (data) {
-  if (typeof data !== 'string') throw new Error(`${data} should be string`)
+  return cityhash.hash64(stringifyObject(data))
+}
 
-  return cityhash.hash64(data)
+function stringifyObject (obj) {
+  if (typeof obj === 'string') return obj
+  if (Buffer.isBuffer(obj)) return obj.toString()
+  if (Array.isArray(obj)) obj = obj.map(sortObject)
+
+  return JSON.stringify(sortObject(obj))
+}
+
+function sortObject (obj) {
+  if (!obj || Array.isArray(obj) || typeof o !== 'object') return obj
+
+  const keys = Object.keys(obj)
+  keys.sort()
+
+  const values = []
+  for (let i = 0; i < keys.length; i++) {
+    const k = keys[i]
+    values.push([k, sortObject(obj[k])])
+  }
+
+  return values
 }
 
 module.exports = { hash32, hash64 }
