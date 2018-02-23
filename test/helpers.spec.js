34c8d723ad685000559b950dddc284889c09fac0
spiritai_david <david@spiritai.com>
2018-02-23 11:58:59 +0000

    Added helpers file and test, with 100% coverage
    Signed-off-by: spiritai_david <david@spiritai.com>

---
+++ test/helpers.spec.js
@@ -0,0 +1,29 @@
+const { stringifyObject } = require('../helpers.js');
+
+describe('helpers', () => {
+  describe('stringifyObject', () => {
+    it('should return a string when given a string', () => {
+      expect(typeof stringifyObject('Hello, World!')).toBe('string');
+    });
+
+    it('should return a string when given a buffer object', () => {
+      const testObj = Buffer.from('Hello, World!');
+      const result = stringifyObject(testObj);
+      expect(typeof result).toBe('string');
+    });
+
+    it('should a string when given an array', () => {
+      const testObj = [{ b: 2 }, { a: 0 }];
+      const result = stringifyObject(testObj);
+      expect(typeof result).toBe('string');
+      expect(result).toBe('[[["b",2]],[["a",0]]]');
+    });
+
+    it('should a sort and return string when given an object', () => {
+      const testObj = [{ b: 2, a: 0 }];
+      const result = stringifyObject(testObj);
+      expect(typeof result).toBe('string');
+      expect(result).toBe('[[["a",0],["b",2]]]');
+    });
+  });
+});
